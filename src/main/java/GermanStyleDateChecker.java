501a786a4158ca63095e7b62d7a1025fbce21888
michaelhel <michael.helcig@mailbox.org>
2018-04-24 21:19:29 +0200

    Added Matcher

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,6 +3,14 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        return null;
+        Pattern p = Pattern.compile("^"
+                + "(([0-2]?[0-9])|(3[0-1]))"
+                + "[\\s.-]"
+                + "((0?[1-9])|(1[0-2]))"
+                + "[\\s.-]"
+                + "[1-2]?(0|9)?[0-9]{2}"
+                + "$");
+        Matcher m = p.matcher(dateString);
+        return m;
     }
 }
